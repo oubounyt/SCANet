dfe0def96fefb660c2c0007ff7e988d88ba9dc3f
TilmanImmisch <58079950+TilmanImmisch@users.noreply.github.com>
2022-09-12 16:23:27 +0200

    everything needed for publishing

---
+++ scannet/__init__.py
@@ -0,0 +1,10 @@
+from .preprocessing import Preprocessing as pp
+from .coexpression import CoExpression as co
+from .pseudobulk import Pseudobulk as pb
+from .grn import GRN as grn
+from .plotting import Visualization as pl
+from .plottingNetworks import PlotNetworks as pln
+
+# rp_poetry/__init__.py
+
+__version__ = "0.1.0"
